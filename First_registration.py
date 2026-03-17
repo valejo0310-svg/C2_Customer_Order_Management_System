44c17a3599a46d93404747ec24bc6259dc554700
Valery Avila <valejo0310@gmail.com>
2026-03-17 10:41:19 -0500

    Update First_registration.py

--- First_registration.py
+++ First_registration.py
@@ -1,9 +1,21 @@
-def customer_registration (customer_ID, client_name, email): 
-#Function created to allow the system to register the client's information
-#Parameters to show all the local variables
-    registration = {
-        "ID" : customer_ID,
-        "customer" : client_name,
-        "email" : email
-    }#Dictionary used to store all the client's data entere (Id, customer's name and email)
-    return registration 
+clients = ()
+def customer_registration (clients):
+    n = int(input("how many clients do you want to register?: "))
+
+    for i in range(n):
+        print("customer", i+1)
+
+        ID = int (input("Customer's ID: "))
+        name = input("Customer's name: ")
+        email = input("Customer's email: ")
+
+        clients = ( ID, name, email)
+
+        clients = clients + (clients,)  # agregamos a la tupla
+
+        print("Customer added successfully", i+1)
+        print("Customer's ID:", ID)
+        print("Customer's name:", name)
+        print("Customer's email:", email)
+
+    return clients
