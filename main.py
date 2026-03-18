356f1d8d40a8a880ed4f9ac0021fa214403b58c8
Leonardo Perez <leonardojeagerpt2005@gmail.com>
2026-03-18 09:36:12 -0500

    update_commit3

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-from First_registration import customer_registration #Importing the registration of the customer's function
-#Creation of the main function
-#Function used to call all the other functions on the main archive
-def main ():
-    print ("""
+from function_registeer import function_register
+
+accumulated_tuple =()
+print ("""
     ==========================================
       ~ welcome to the registration system ~ 
     ==========================================
-    """.center(60)) #Welcome message to the system
-    #Variables created to store all the client's data such as Id, names and emails
-    customer_ID = input ("Please enter customer's ID:")
-    client_name = input ("\nPlease enter the customer's name: ")
-    email = input ("\nPlease enter the customer´s email: ")
-    #Creation of the variable "customer" to call the registration function
-    customer = customer_registration(customer_ID, client_name, email) 
-    print (customer)#Print used to show the variable that contains the costumer's info
-
-
-
-main ()#Calling the main function
+    """.center(60))
+accumulated_tuple = function_register (accumulated_tuple)
+print (accumulated_tuple)
