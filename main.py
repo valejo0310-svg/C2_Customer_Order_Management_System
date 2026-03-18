4238865cb1fc20cebf534b29e747680f30b834b5
Valery Avila <valejo0310@gmail.com>
2026-03-18 07:38:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from First_registration import customer_registration  #Importing the registration of the customer's function
 #Creation of the main function
 #Function used to call all the other functions on the main archive
-clients =()
+accumulated_tuple =()
 print ("""
     ==========================================
       ~ welcome to the registration system ~ 
     ==========================================
     """.center(60))
-clients = customer_registration (clients)
+accumulated_tuple = customer_registration (accumulated_tuple)
+print (accumulated_tuple)
