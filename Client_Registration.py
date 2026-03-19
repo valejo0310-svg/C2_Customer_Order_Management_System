6a7126ac65702db3eac9fc8bdc5a69fa67b69bba
Valery Avila <valejo0310@gmail.com>
2026-03-18 21:50:30 -0500

    Fix tuple syntax in Client_Registration.py

--- Client_Registration.py
+++ Client_Registration.py
@@ -3,7 +3,7 @@ def customer_registration (accumulated_tuple):
     "Customer's ID " : "0001",
     "Customer's name " : "Guest", 
     "Customer's Email" : "Guest0001@gmail.com"
-})
+},)
     n = int(input("how many clients do you want to register?: "))
     print ("CUSTOMER REGISTRATION")
     for i in range(n):
