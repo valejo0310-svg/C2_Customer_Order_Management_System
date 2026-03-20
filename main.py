79b910f47b72addc60416e75b6d28e7ad7a2b3ac
Valery Avila <valejo0310@gmail.com>
2026-03-19 23:30:21 -0500

    Refactor main.py for improved readability and structure

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
-#from First_registration import customer_registration #Importing the registration of the customer's function
+#Importing functions from other files
+from First_registration import customer_registration
+from function_register import function_register
+from income_calculation import income_calculation
+from view_orders import view_orders
+from final_report import final_report
+from create_order import create_order
 
-print (f"""
+#Initializing data structures that are gonna be used in the program
+customers = {}
+products = ()
+orders = {}
+#Welcome message
+print(f"""
 ╔═══════════════════════════════════════════════════════════╗
                 🥑 RIWIMART RETAIL CHAIN 🥑
    🌷 Welcome to the Customer Order Management System! 🌷
-╚═══════════════════════════════════════════════════════════╝""") #Some text formatting is used to make the welcome message more attractive
+╚═══════════════════════════════════════════════════════════╝
+""")
+#Main menu loop
 Menu = 1
-while Menu:#Loop created to keep asking the user when they enter a wrong option
-    try:          #Try and except created to catch the error when the user enters a non-integer value    
-        Option = int(input(f"""{"-"*24} MAIN MENU {"-"*25} 
-1) 🤵​  Enter Costumer 
-2) 🍎  Enter Product 
-3) ​🧾​  Create Order
-4) ​🔍​  View Orders
-5) 📋​  Generate Final Report
-6) 💀​  Exit
+#The main menu loop will continue until the user chooses to exit by selecting option 6.
+# Inside the loop, the program will prompt the user to select an option 
+
+while Menu:
+    try:
+        option = int(input(f"""{"-"*24} MAIN MENU {"-"*25}
+1) 🤵 Enter Customer
+2) 🍎 Enter Product
+3) 🧾 Create Order
+4) 🔍 View Orders
+5) 📋 Generate Final Report
+6) 💀 Exit
 {"="*60}
-~ Please select an option:
-➤  """)) # Some text formatting is used to make the menu more attractive  
-        if Option < 1 or Option > 6: #Condition created to catch the error when the user enters a number that is not on the menu
-            print(f"\n{'❌ ERROR: Please enter a valid number ❌':^60}\n") 
-            Option = int(input(f"""{"-"*24} MAIN MENU {"-"*25} 
-1) 🤵​  Enter Costumer 
-2) 🍎  Enter Product 
-3) ​🧾​  Create Order 
-4) ​🔍  View Orders
-5) 📋​  Generate Final Report
-6) 💀​  Exit
-{"="*60}
-~ Please select an option:
-➤  """))      
+➤ """))
+# Depending on the user's choice, the program will execute the corresponding function:
+        if option == 1:
+            customer = customer_registration()
+            customers[customer["ID"]] = customer
+            print("Customer registered!\n")
+
+        elif option == 2:
+            products = function_register(products)
+
+        elif option == 3:
+            orders = create_order(orders, customers, products)
+
+        elif option == 4:
+            view_orders(orders, customers, products)
+
+        elif option == 5:
+            final_report(orders, customers)
+
+        elif option == 6:
+            print("Exiting...")
+            Menu = 0
+
+        else:
+            print("Option not available.\n")
+
     except ValueError:
-        print(f"\n{'❌ ERROR: Please enter a valid integer ❌':^60}\n")
-
-#First condition created to call the function of the customer's registration when the user selects the first option of the menu.
-#if Option == 1:
-
+        print("❌ Invalid number.\n")
